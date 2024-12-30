75a902a64be385ea94604ffa22d43138077fe00e
Brynn Warner <brynn@brynnsoft.com>
2024-12-30 01:01:25 +0000

    Fix has revision mixin

--- pyrewall/core/db/mixins/has_revision.py
+++ pyrewall/core/db/mixins/has_revision.py
@@ -2,5 +2,5 @@ from sqlalchemy import Boolean, ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped
 
 class HasRevisionMixin():
-    revision_id: Mapped[int] = mapped_column(ForeignKey('revision.id'), index=True)
+    revision_id: Mapped[int] = mapped_column(ForeignKey('revisions.id'), index=True)
     enabled: Mapped[bool] = mapped_column(Boolean)
