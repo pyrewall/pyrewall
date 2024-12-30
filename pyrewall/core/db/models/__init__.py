c00ace7ac1304b3f26257fe511145725863338c8
Brynn Warner <brynn@brynnsoft.com>
2024-12-30 01:00:42 +0000

    Add initial version of revisions table

--- pyrewall/core/db/models/__init__.py
+++ pyrewall/core/db/models/__init__.py
@@ -1,4 +1,5 @@
 import pyrewall.core.db.models.auth_source
 import pyrewall.core.db.models.group
+import pyrewall.core.db.models.revision
 import pyrewall.core.db.models.user
 import pyrewall.core.db.models.user_groups
--- pyrewall/core/db/models/revision.py
+++ pyrewall/core/db/models/revision.py
@@ -1,13 +1,16 @@
 from datetime import datetime
-from sqlalchemy import Integer, DateTime, Boolean
+from uuid import UUID
+from sqlalchemy import Integer, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base_model import Base
 
 class Revision(Base):
-    __tablename__ = 'revision'
+    __tablename__ = 'revisions'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, index=True, unique=True, nullable=False)
+    previous_revision_id: Mapped[int] = mapped_column(ForeignKey('revisions.id'), nullable=True)
+    owner_id: Mapped[UUID] = mapped_column(ForeignKey('users.id'), nullable=False)
     created_date: Mapped[datetime] = mapped_column(DateTime, insert_default=datetime.now(), default=None, nullable=False)
     commit_date: Mapped[datetime] = mapped_column(DateTime, default=None, nullable=True)
     applied_date: Mapped[datetime] = mapped_column(DateTime, default=None, nullable=True)
